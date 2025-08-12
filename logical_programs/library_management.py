0b56043263b10e65b429b693706e39f985b2bb27
Siddhu <siddarthsiddhu5100@gmail.com>
2025-08-12 11:14:35 +0530

    new programs

---
+++ logical_programs/library_management.py
@@ -0,0 +1,38 @@
+# objective to store book details, search/filter, update/remove entries. 
+
+# data structure
+
+books = {"B1001" : {"title" : "The Great Gatsby",
+                    "author" : "F. Scott Fitzgerald",
+                    "year" : 1925,
+                    "available" : True
+    },
+    "B1002" : {"title" : "1984",
+               "author" : "George Orwell",
+               "year" : 1949,
+               "available" : False
+
+    }
+}
+
+def add_a_book():
+    book_id = input("Enter the id of the book :")
+    title = input("Enter the title of the book :")
+    author = input("Enter name of the author :")
+    year = input("Enter year :")
+    available_input = input("Is available yes/no :")
+    available = True if available_input == "yes" else False 
+    
+    books[book_id] = {"title":title, 
+                      "author":author, 
+                      "year":year, 
+                      "available":available
+}
+    print("Book added successfully!")
+    print(books)
+
+
+add_a_book()
+
+
+
--- rough_practice/testing_json.py
+++
@@ -1,7 +0,0 @@
-import json
-
-with open("students.json", "r") as f:
-    loaded_students = json.load(f)
-
-print("Imported data from JSON:")
-print(loaded_students)
